a4c8cd76a0461b416b56ab77654ce2e0015edc8d
Madhumitha <madhumithaanjel@gmail.com>
2026-08-05 14:56:36 +0530

    Add vowel extraction functionality in program4.java
    Implement a program to print vowels from input string.

---
+++ String/program4.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main
+{
+	public staticA void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		String s=sc.nextLine();
+		vowel(s);
+	}
+	public static void vowel(String s){
+	    for(int i=0;i<s.length();i++){
+	        char ch=s.charAt(i);
+	        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'){
+	        System.out.print(ch);
+	        }
+	    }
+	}
+}
